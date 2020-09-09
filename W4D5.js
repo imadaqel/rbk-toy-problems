c37ae0d9740ed669ecaa61755e5fd87d90ce9d95
imadaqel <emadaqel92@gmail.com>
2020-09-09 09:55:38 +0300

    origin

--- W4D5.js
+++ W4D5.js
@@ -19,3 +19,30 @@ Would return:
 
 */
 
+var staff = [
+  {name: {first: "Alyssa", last: "Hacker"}, birthDay: {month: "June", day: 5, year: 1987}},
+  {name: {first: "Ben", last: "Bitdiddle"}, birthDay: {month: "August", day: 19, year: 1984}},
+  {name: {first: "Eva", last: "Ator"}, birthDay: {month: "March", day: 29, year: 1980}},
+  {name: {first: "Lem", last: "Tweakit"}, birthDay: {month: "August", day: 11, year: 1989}},
+  {name: {first: "Louis", last: "Reasoner"}, birthDay: {month: "November", day: 17, year: 1992}}
+];
+
+
+
+function getPeopleBornIn(array,string){
+  var arr=[]
+   for(var i=0;i<array.length;i++){
+
+    if(array[i].birthDay.month===string){
+
+     var x= array[i].name.first +' '+ array[i].name.last+ ":" + array[i].birthDay.month +' '+ array[i].birthDay.year 
+
+     arr.push(x)
+    }
+
+  
+   }
+ 
+ return arr;
+}
+getPeopleBornIn(staff,'August')
