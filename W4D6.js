a89609f904eeb013dc0b8db484aff3a3c2f91905
imadaqel <emadaqel92@gmail.com>
2020-09-13 09:39:28 +0300

    origin

--- W4D6.js
+++ W4D6.js
@@ -1,5 +1,69 @@
 //Create a function (class) called Student that return an object that contains the following properties:
 //Name, Date of Birth, Gender, Age
+
+function Student(name,date,gender,age){
+ var student={}
+ student.name=name;
+ student.date=date;
+ student.gender=gender;
+ student.age=age;
+ student.printout=printout;
+ student.sort=sort;
+ student.gendersort=gendersort;
+ //array.push(student)
+ return student;
+ //console.log(array)
+}
+
+
+//Student('ahmad',20.3,'male',20)
+ 
+ function printout(student){
+
+   return student.name + ' ' + student.date + ' '+ student.gender
+ }
+   
+   function sort(students){
+    
+    
+    var result= students.sort(function(a, b){
+
+       return a.age-b.age;
+     })
+     return result;
+   }
+    
+    function gendersort(array,string){
+     var arr=[]
+     
+     for(var i=0;i<array.length;i++){
+
+
+
+     }
+
+     if(string==='m' && element.gender==='male'){
+
+      arr.push(element)
+
+     }
+if(string==='f' && element.gender==='female'){
+
+      arr.push(element)
+
+     }
+     })
+     return arr
+      }
+
+ //printout(student)
+var arrayst=[Student('imad',10.2,'male',23),
+Student('ahmad',1.2,'male',20),Student('ah',1.2,'male',28)]
+
+ //printout(arrayst[1])
+gendersort(arrayst,'m')
+
+
 //Create an array and fill it with 5 students
 //Create a function that takes a student as parameter and print out the details of the student
 //Create a function that takes the array of students and sort them out based on age (younger-to-older)
--- W5D2.js
+++ W5D2.js
@@ -2,8 +2,16 @@
 1) implement the last version of each 
 */
 
-function each(coll, func) {
- //write your code here  
+function each(coll, f) {
+  if (Array.isArray(coll)) {
+    for (var i = 0; i < coll.length; i++) {
+      f(coll[i], i);
+    }
+  } else {
+    for (var key in coll) {
+      f(coll[key], key);
+    }
+  }
 }
 	
 
@@ -11,19 +19,27 @@ function each(coll, func) {
 1) implement map function
 
 */
-	function map(array, f) {
-	  var acc = [];
-	  
-	  return acc;
-	}
+	function map(array, func) {
+  var acc = [];
+  each(array, function(element, i) {
+    acc.push(func(element, i));
+  });
+  return acc;
+}
+}
 
 /*
 1) implement the improved map 
 */
+  
+  function map(coll, f) {
+  var acc = [];
+ if(Array.isArray(coll)){
+ 	acc={}
+ }
+ each(coll, function(element, key){
+  acc[key]=f(element, key);
 
-
-function map(coll, f) {
-	var acc = [];
-	  
-	 return acc;
+ })
+ return acc;
 }
